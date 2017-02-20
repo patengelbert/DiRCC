6ae0bcc6d97313f379b0d507dfd363647309f76d
Patrick Engelbert <patengelbert@hotmail.co.uk>
2017-02-20 19:08:55 +0000

    Added logging to onidle

--- software/dircc/src/dircc_on_idle.c
+++ software/dircc/src/dircc_on_idle.c
@@ -4,12 +4,15 @@
 bool dircc_onIdle(PThreadContext *ctxt)
 {
     DeviceContext *dev=0;
-
-    if(ctxt->rtcChecked >= ctxt->numDevices)
+    DIRCC_LOG_PRINTF("previously checked %u/%u nodes", ctxt->rtcChecked, ctxt->numDevices);
+    if(ctxt->rtcChecked >= ctxt->numDevices) {
+    	DIRCC_LOG_PRINTF("No computes available");
         return false; // We already did a complete loop and there was nothing
+    }
 
     for(unsigned i=0; i<IDLE_SWEEP_CHUNK_SIZE;i++){
         dev=ctxt->devices + ctxt->rtcOffset;
+    	DIRCC_LOG_PRINTF("checking device %u", dev->index);
 
         ctxt->rtcOffset++;
         ctxt->rtcChecked++;
@@ -18,15 +21,19 @@ bool dircc_onIdle(PThreadContext *ctxt)
         }
 
         if( dev->rtsFlags&0x80000000 ){
+            DIRCC_LOG_PRINTF("device %u wishes to compute", dev->index);
             break;
         }
     }
 
-    if(dev)
+    if(dev) {
+    	DIRCC_LOG_PRINTF("couldn't find device this time");
         return true; // Didn't find one this time, but there might still be one
+    }
 
     const DeviceTypeVTable *vtable=dev->vtable;
 
+    DIRCC_LOG_PRINTF("calling computeHandler");
     vtable->computeHandler(
         ctxt->graphProps,
         dev->properties,
