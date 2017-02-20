ed77273ecacb54fd655fa583edf6dd0b09afe87f
Patrick Engelbert <patengelbert@hotmail.co.uk>
2017-02-20 19:09:10 +0000

    Formatted fifo interface

--- software/dircc/include/dircc_fifo_interface.h
+++ software/dircc/include/dircc_fifo_interface.h
@@ -1,22 +1,25 @@
-#ifndef DIRCC_INTERFACE
-#define DIRCC_INTERFACE
+#ifndef DIRCC_FIFO_INTERFACE
+#define DIRCC_FIFO_INTERFACE
 
 #include "dircc.h"
 
 uint32_t dircc_my_id();
 
-dircc_err_code dircc_init_fifo(uint32_t csr_address, uint32_t almost_empty, uint32_t almost_full);
+dircc_err_code dircc_init_fifo(uint32_t csr_address, uint32_t almost_empty,
+		uint32_t almost_full);
 
 dircc_err_code dircc_can_send();
 
-dircc_err_code dircc_send(uint32_t data_address, uint32_t csr_address, const packet_t* msg);
+dircc_err_code dircc_send(uint32_t data_address, uint32_t csr_address,
+		const packet_t* msg);
 
 dircc_err_code dircc_can_recv();
 
-dircc_err_code dircc_recv(uint32_t data_address, uint32_t csr_address, packet_t* msg);
+dircc_err_code dircc_recv(uint32_t data_address, uint32_t csr_address,
+		packet_t* msg);
 
 dircc_err_code dircc_clr_fifo(uint32_t data_address, uint32_t csr_address);
 
 uint32_t dircc_get_level(uint32_t csr_address);
 
-#endif // !DIRCC_INTERFACE
+#endif // !DIRCC_FIFO_INTERFACE
