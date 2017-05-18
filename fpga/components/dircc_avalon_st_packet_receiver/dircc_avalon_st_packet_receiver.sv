7d9b06db99cf17223af00a7fba5b0ebde23e7af6
Patrick Engelbert <patengelbert@hotmail.co.uk>
2017-05-18 17:34:15 +0100

    Added packet receiver module

---
+++ fpga/components/dircc_avalon_st_packet_receiver/dircc_avalon_st_packet_receiver.sv
@@ -0,0 +1,174 @@
+
+module dircc_avalon_st_packet_receiver(
+  clk,
+  reset_n,
+
+  data,
+  empty,
+  startofpacket,
+  endofpacket,
+  ready,
+  valid,
+
+  receiving
+);
+
+  parameter BITS_PER_SYMBOL = 8;
+  parameter SYMBOLS_PER_BEAT = 4;
+  parameter INPUT_FIFO_DEPTH = 8;
+  localparam DATA_WIDTH = BITS_PER_SYMBOL * SYMBOLS_PER_BEAT;
+  localparam EMPTY_WIDTH = $clog2(SYMBOLS_PER_BEAT);
+  localparam NULL_BYTE = 8'b0;
+
+  input  wire                   clk;
+  input  wire                   reset_n;
+  
+  input wire [DATA_WIDTH-1:0]   data;
+  input wire [EMPTY_WIDTH-1:0]  empty;
+  input wire                    startofpacket;
+  input wire                    endofpacket;
+  output reg                    ready;
+  input wire                    valid;
+
+  output wire                   receiving;
+
+  import dircc_types_pkg::*;
+  
+  typedef enum logic[5:0] {IDLE, DEST_ADDR0, DEST_ADDR1, SRC_ADDR0, SRC_ADDR1, LAMPORT, DATA0, DATA1, DATA2} packetState_t;
+  
+  wire [DATA_WIDTH-1:0]   input_fifo_out_data;
+  wire [EMPTY_WIDTH-1:0]  input_fifo_out_empty;
+  wire                    input_fifo_out_startofpacket;
+  wire                    input_fifo_out_endofpacket;
+  reg                     input_fifo_out_ready;
+  wire                    input_fifo_out_valid;
+
+  bool packetDone;
+
+  packet_t currentPacket;
+  packetState_t packetState;
+
+  assign receiving = (packetDone == FALSE) ? 1 : 0;
+
+  // assign input_fifo_out_data = data;
+  // assign input_fifo_out_empty = empty;
+  // assign input_fifo_out_endofpacket = endofpacket;
+  // assign ready = input_fifo_out_ready;
+  // assign input_fifo_out_startofpacket = startofpacket;
+  // assign input_fifo_out_valid = valid;
+
+  altera_avalon_sc_fifo #(
+		.SYMBOLS_PER_BEAT    (SYMBOLS_PER_BEAT),
+		.BITS_PER_SYMBOL     (BITS_PER_SYMBOL),
+		.FIFO_DEPTH          (INPUT_FIFO_DEPTH),
+		.CHANNEL_WIDTH       (0),
+		.ERROR_WIDTH         (0),
+		.USE_PACKETS         (1),
+		.USE_FILL_LEVEL      (0),
+		.EMPTY_LATENCY       (1),
+		.USE_MEMORY_BLOCKS   (1),
+		.USE_STORE_FORWARD   (0),
+		.USE_ALMOST_FULL_IF  (0),
+		.USE_ALMOST_EMPTY_IF (0)
+	) input_fifo (
+		.clk               (clk),                                  //       clk.clk
+		.reset             (~reset_n),                              // clk_reset.reset
+		.in_data           (data),                                 //        in.data
+		.in_valid          (valid),                                //          .valid
+		.in_ready          (ready),                                //          .ready
+		.in_startofpacket  (startofpacket),                        //          .startofpacket
+		.in_endofpacket    (endofpacket),                          //          .endofpacket
+		.in_empty          (empty),                                //          .empty
+		.out_data          (input_fifo_out_data),                  //       out.data
+		.out_valid         (input_fifo_out_valid),                 //          .valid
+		.out_ready         (input_fifo_out_ready),                 //          .ready
+		.out_startofpacket (input_fifo_out_startofpacket),         //          .startofpacket
+		.out_endofpacket   (input_fifo_out_endofpacket),           //          .endofpacket
+		.out_empty         (input_fifo_out_empty),                 //          .empty
+		.csr_address       (2'b00),                                // (terminated)
+		.csr_read          (1'b0),                                 // (terminated)
+		.csr_write         (1'b0),                                 // (terminated)
+		.csr_readdata      (),                                     // (terminated)
+		.csr_writedata     (32'b00000000000000000000000000000000), // (terminated)
+		.almost_full_data  (),                                     // (terminated)
+		.almost_empty_data (),                                     // (terminated)
+		.in_error          (1'b0),                                 // (terminated)
+		.out_error         (),                                     // (terminated)
+		.in_channel        (1'b0),                                 // (terminated)
+		.out_channel       ()                                      // (terminated)
+	);
+  
+  always_ff @(posedge clk or negedge reset_n) begin
+    if(!reset_n) begin
+      input_fifo_out_ready <= 0;
+      packetDone <= FALSE;
+      packetState <= DEST_ADDR0;
+    end
+    else if (packetDone == FALSE) begin
+      input_fifo_out_ready <= 1;
+      case(packetState)
+        DEST_ADDR0: begin
+          if (input_fifo_out_valid) begin
+            assert(input_fifo_out_startofpacket);
+            currentPacket.dest_addr.hw_addr <= input_fifo_out_data;
+            packetState <= DEST_ADDR1;
+          end
+        end
+        DEST_ADDR1: begin
+          if (input_fifo_out_valid) begin
+            {currentPacket.dest_addr.sw_addr, currentPacket.dest_addr.port, currentPacket.dest_addr.flag} <= input_fifo_out_data[31:8];
+            packetState <= SRC_ADDR0;
+          end
+        end
+        SRC_ADDR0: begin
+          if (input_fifo_out_valid) begin
+            currentPacket.src_addr.hw_addr <= input_fifo_out_data;
+            packetState <= SRC_ADDR1;
+          end
+        end
+        SRC_ADDR1: begin
+          if (input_fifo_out_valid) begin
+            {currentPacket.src_addr.sw_addr, currentPacket.src_addr.port, currentPacket.src_addr.flag} <= input_fifo_out_data[31:8];
+            packetState <= LAMPORT;
+          end
+        end
+        LAMPORT: begin
+          if (input_fifo_out_valid) begin
+            currentPacket.lamport = input_fifo_out_data;
+            packetState <= DATA0;
+          end
+        end
+        DATA0: begin
+          if (input_fifo_out_valid) begin
+            currentPacket.data[31:0] = input_fifo_out_data;
+            packetState <= DATA1;
+          end
+        end
+        DATA1: begin
+          if (input_fifo_out_valid) begin
+            currentPacket.data[63:32] = input_fifo_out_data;
+            packetState <= DATA2;
+          end
+        end
+        DATA2: begin
+          if (input_fifo_out_valid) begin
+            assert(input_fifo_out_endofpacket);
+            currentPacket.data[95:64] = input_fifo_out_data;
+            packetState <= DEST_ADDR0;
+            packetDone <= TRUE;
+            input_fifo_out_ready <= 0;
+          end
+        end
+      endcase
+    end else begin
+      input_fifo_out_ready <= 0;
+      packetState <= DEST_ADDR0;
+    end
+  end
+  
+  task receivePacket(output packet_t packet);
+    packet = currentPacket;
+    packetDone = FALSE;
+  endtask : receivePacket
+  
+endmodule : dircc_avalon_st_packet_receiver
---
+++ fpga/components/dircc_avalon_st_packet_receiver/testbench/dircc_avalon_st_packet_receiver_tb/simulation/dircc_avalon_st_packet_receiver_test.sv
@@ -0,0 +1,288 @@
+
+`timescale 1 ps / 1 ps
+
+module dircc_avalon_st_packet_receiver_test;
+    parameter TIMEOUT = 10000;
+
+    dircc_avalon_st_packet_receiver_tb tb();
+
+    string message = "*uninitialized*";
+
+    import dircc_types_pkg::*;
+    import verbosity_pkg::*;
+
+    alias reset = tb.dircc_avalon_st_packet_receiver_inst_reset_bfm_reset_reset;
+    alias clk = tb.dircc_avalon_st_packet_receiver_inst_clk_bfm_clk_clk;
+
+    event reset_asserted, reset_deasserted, receiving_done;
+
+    always @(negedge tb.dircc_avalon_st_packet_receiver_inst_receiving) begin
+        -> receiving_done;
+    end
+
+    task automatic setupTest();
+        tb.dircc_avalon_st_packet_receiver_inst_reset_bfm.reset_assert();
+        -> reset_asserted;
+        repeat(10) @(posedge clk);
+        tb.dircc_avalon_st_packet_receiver_inst_reset_bfm.reset_deassert();
+        repeat(2) @(posedge clk);
+        -> reset_deasserted;
+    endtask : setupTest
+
+    task automatic waitWithTimeout(input event ev, input int timeout);
+        fork: waitWithTimeoutFork 
+            begin : checkReceived
+                wait(ev.triggered);
+                disable waitWithTimeoutFork;
+            end : checkReceived
+            begin : checkTimout
+                repeat(timeout) @(posedge clk);
+                $sformat(message, "%m: - TEST Timeout %d", timeout);
+                print(VERBOSITY_ERROR, message);  
+                disable waitWithTimeoutFork;
+            end : checkTimout 
+        join : waitWithTimeoutFork
+    endtask : waitWithTimeout
+
+    task automatic sendPacket(packet_t packet);
+        // Word 1
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_data(packet.dest_addr.hw_addr);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_sop(1);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_eop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_empty(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.push_transaction();
+        
+        // Word 2
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_data(
+            {packet.dest_addr.sw_addr, packet.dest_addr.port, packet.dest_addr.flag, 8'b0});
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_sop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_eop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_empty(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.push_transaction();
+
+        // Word 3
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_data(packet.src_addr.hw_addr);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_sop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_eop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_empty(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.push_transaction();
+
+        // Word 4
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_data(
+            {packet.src_addr.sw_addr, packet.src_addr.port, packet.src_addr.flag, 8'b0});
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_sop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_eop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_empty(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.push_transaction();
+
+        // Word 5
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_data(packet.lamport);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_sop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_eop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_empty(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.push_transaction();
+
+        // Word 6
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_data(packet.data[31:0]);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_sop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_eop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_empty(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.push_transaction();
+
+        // Word 7
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_data(packet.data[63:32]);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_sop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_eop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_empty(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.push_transaction();
+
+        // Word 8
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_data(packet.data[95:64]);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_sop(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_eop(1);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.set_transaction_empty(0);
+        tb.dircc_avalon_st_packet_receiver_inst_input_bfm.push_transaction();
+
+    endtask : sendPacket
+
+    task automatic checkPacket(packet_t a, packet_t b, output bool rv);
+        rv = TRUE;
+        // Dest Address
+        assert (a.dest_addr.hw_addr == b.dest_addr.hw_addr)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Dest HW Addr mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.dest_addr.sw_addr == b.dest_addr.sw_addr)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Dest SW Addr mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.dest_addr.port == b.dest_addr.port)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Dest Port mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.dest_addr.flag == b.dest_addr.flag)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Dest Flag mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+
+        // Src Address
+        assert (a.src_addr.hw_addr == b.src_addr.hw_addr)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Src HW Addr mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+
+        assert (a.src_addr.sw_addr == b.src_addr.sw_addr)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Src SW Addr mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.src_addr.port == b.src_addr.port)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Src Port mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.src_addr.flag == b.src_addr.flag)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Src Flagismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+
+        // lamport
+        assert (a.lamport == b.lamport)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Lamport mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+
+        // Data
+        assert (a.data == b.data)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Data mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+
+    endtask : checkPacket
+
+    task automatic test_resetCorrect();
+        automatic bool rv = FALSE;
+        automatic packet_t packetToSend = '{
+            dest_addr: '{
+                hw_addr: $random,
+                sw_addr: $random,
+                port: $random,
+                flag: 0
+            },
+            src_addr: '{
+                hw_addr: $random,
+                sw_addr: $random,
+                port: $random,
+                flag: 0
+            },
+            lamport: 0,
+            data: $random
+        };
+        setupTest();
+        tb.dircc_avalon_st_packet_receiver_inst_reset_bfm.reset_assert();
+        repeat(10) @(posedge clk);
+        tb.dircc_avalon_st_packet_receiver_inst_reset_bfm.reset_deassert();
+        repeat(10) @(posedge clk);
+        assert (tb.dircc_avalon_st_packet_receiver_inst_receiving)
+        else begin
+            $sformat(message, "%m: - TEST ERROR");
+            print(VERBOSITY_ERROR, message);  
+        end
+        sendPacket(packetToSend);
+        waitWithTimeout(tb.dircc_avalon_st_packet_receiver_inst_input_bfm.signal_src_transaction_complete, TIMEOUT);
+
+        rv = tb.dircc_avalon_st_packet_receiver_inst_input_bfm.get_transaction_queue_size() == 0 ? TRUE : FALSE;
+
+        assert(rv == TRUE) begin
+            $sformat(message, "%m: - TEST SUCCESS");
+            print(VERBOSITY_INFO, message);  
+        end else begin
+            $sformat(message, "%m: - TEST ERROR");
+            print(VERBOSITY_ERROR, message);  
+        end
+
+        $sformat(message, "%m: - Test Complete.");
+        print(VERBOSITY_INFO, message);  
+    endtask : test_resetCorrect
+
+    task automatic test_receivePacket();
+        automatic bool rv = FALSE;
+        automatic packet_t packetToSend = '{
+            dest_addr: '{
+                hw_addr: $random,
+                sw_addr: $random,
+                port: $random,
+                flag: 0
+            },
+            src_addr: '{
+                hw_addr: $random,
+                sw_addr: $random,
+                port: $random,
+                flag: 0
+            },
+            lamport: 0,
+            data: $random
+        };
+        automatic packet_t receivedPacket;
+        setupTest();
+
+        sendPacket(packetToSend);
+        waitWithTimeout(tb.dircc_avalon_st_packet_receiver_inst_input_bfm.signal_src_transaction_complete, TIMEOUT);
+
+        rv = tb.dircc_avalon_st_packet_receiver_inst_input_bfm.get_transaction_queue_size() == 0 ? TRUE : FALSE;
+
+        assert(rv == TRUE) else begin
+            $sformat(message, "%m: - TEST ERROR");
+            print(VERBOSITY_ERROR, message);  
+        end
+        
+        waitWithTimeout(receiving_done, TIMEOUT);
+
+        if(!tb.dircc_avalon_st_packet_receiver_inst.receiving) begin
+            tb.dircc_avalon_st_packet_receiver_inst.receivePacket(receivedPacket);
+            checkPacket (receivedPacket, packetToSend, rv);
+        end else begin
+            $sformat(message, "%m: - Packet not fully received");
+            print(VERBOSITY_ERROR, message);  
+            rv = FALSE;
+        end
+
+        assert(rv == TRUE) begin
+            $sformat(message, "%m: - TEST SUCCESS");
+            print(VERBOSITY_INFO, message);  
+        end else begin
+            $sformat(message, "%m: - TEST ERROR");
+            print(VERBOSITY_ERROR, message);  
+        end
+
+        $sformat(message, "%m: - Test Complete.");
+        print(VERBOSITY_INFO, message);  
+    endtask : test_receivePacket
+
+    initial begin
+        repeat(100) @(posedge clk);
+        $sformat(message, "%m: - Starting Tests.");
+        print(VERBOSITY_INFO, message);  
+        test_resetCorrect();
+        test_receivePacket();
+    end
+
+endmodule : dircc_avalon_st_packet_receiver_test
