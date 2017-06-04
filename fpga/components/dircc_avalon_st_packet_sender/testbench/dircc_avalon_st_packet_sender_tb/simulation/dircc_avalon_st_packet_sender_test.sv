fc5e844263d9e88916284a9552fd4bf5ea71c0cd
Patrick Engelbert <patengelbert@hotmail.co.uk>
2017-06-04 20:16:24 +0100

    Added backpressure test and updated tests using new functions

--- fpga/components/dircc_avalon_st_packet_sender/testbench/dircc_avalon_st_packet_sender_tb/simulation/dircc_avalon_st_packet_sender_test.sv
+++ fpga/components/dircc_avalon_st_packet_sender/testbench/dircc_avalon_st_packet_sender_tb/simulation/dircc_avalon_st_packet_sender_test.sv
@@ -27,7 +27,7 @@ module dircc_avalon_st_packet_sender_test;
         -> reset_deasserted;
     endtask : setupTest    
 
-    task automatic waitForMessage(input int timeout, output bool rv);
+    task automatic waitForMessage(input int timeout, inout bool rv);
         fork: WaitWithTimeout 
             begin : checkReceived
                 repeat(8) @(tb.dircc_avalon_st_packet_sender_inst_output_bfm.signal_transaction_received);
@@ -39,70 +39,200 @@ module dircc_avalon_st_packet_sender_test;
                 repeat(timeout) @(posedge clk);
                 $sformat(message, "%m: - TEST Timeout %d. %d/8 received", timeout, tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_queue_size());
                 print(VERBOSITY_ERROR, message);  
+                rv = FALSE;
                 disable WaitWithTimeout;
             end : checkTimout 
         join : WaitWithTimeout
-        rv = tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_queue_size() == 8 ? TRUE : FALSE;
     endtask : waitForMessage
 
-    task automatic checkPacket(packet_t packet);
+    task automatic printSuccess(input bool rv);
+        if (rv == TRUE) begin
+            $sformat(message, "%m: TEST SUCCESS");
+            print(VERBOSITY_INFO, message);
+        end else begin
+            $sformat(message, "%m: TEST ERROR");
+            print(VERBOSITY_ERROR, message);
+        end
+        $sformat(message, "%m: Test Complete");
+        print(VERBOSITY_INFO, message);
+    endtask
+
+    task automatic printAssert(input longint a, input longint b, inout bool rv);
+        assert(a == b)
+        else begin
+            rv = FALSE;
+            $sformat(message, "%m: - Expected: %d. Got %d", a, b);
+            print(VERBOSITY_ERROR, message);
+        end
+
+    endtask
+
+    task automatic receivePacket(output packet_t packet, inout bool rv);
         // Word 1
         tb.dircc_avalon_st_packet_sender_inst_output_bfm.pop_transaction();
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data() == packet.dest_addr.hw_addr);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 1);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0);
+        packet.dest_addr.hw_addr = tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data();
+        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 1
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0)
+        else begin
+            $sformat(message, "%m: - TEST ERROR: Word 0 Metadata mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end
         
         // Word 2
         tb.dircc_avalon_st_packet_sender_inst_output_bfm.pop_transaction();
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data()[31:8] == 
-            {packet.dest_addr.sw_addr, packet.dest_addr.port, packet.dest_addr.flag});
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0);
-
+        {packet.dest_addr.sw_addr, packet.dest_addr.port, packet.dest_addr.flag} = 
+        tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data()[31:8];
+        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0)
+        else begin
+            $sformat(message, "%m: - TEST ERROR: Word 1 Metadata mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end
         // Word 3
         tb.dircc_avalon_st_packet_sender_inst_output_bfm.pop_transaction();
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data() == packet.src_addr.hw_addr);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0);
+        packet.src_addr.hw_addr = tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data();
+        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0)
+        else begin
+            $sformat(message, "%m: - TEST ERROR: Word 2 Metadata mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end
 
         // Word 4
         tb.dircc_avalon_st_packet_sender_inst_output_bfm.pop_transaction();
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data()[31:8] == 
-            {packet.src_addr.sw_addr, packet.src_addr.port, packet.src_addr.flag});
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0);
-
+        {packet.src_addr.sw_addr, packet.src_addr.port, packet.src_addr.flag} = 
+        tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data()[31:8];
+        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0)
+        else begin
+            $sformat(message, "%m: - TEST ERROR: Word 3 Metadata mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end
         // Word 5
         tb.dircc_avalon_st_packet_sender_inst_output_bfm.pop_transaction();
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data() == packet.lamport);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0);
+        packet.lamport = tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data();
+        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0)
+        else begin
+            $sformat(message, "%m: - TEST ERROR: Word 4 Metadata mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end
 
         // Word 6
         tb.dircc_avalon_st_packet_sender_inst_output_bfm.pop_transaction();
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data() == packet.data[31:0]);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0);
+        packet.data[31:0] = tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data();
+        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0)
+        else begin
+            $sformat(message, "%m: - TEST ERROR: Word 5 Metadata mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end
 
         // Word 7
         tb.dircc_avalon_st_packet_sender_inst_output_bfm.pop_transaction();
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data() == packet.data[63:32]);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0);
-
+        packet.data[63:32] = tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data();
+        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0)
+        else begin
+            $sformat(message, "%m: - TEST ERROR: Word 6 Metadata mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end
         // Word 8
         tb.dircc_avalon_st_packet_sender_inst_output_bfm.pop_transaction();
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data() == packet.data[95:64]);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 1);
-        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0);
+        packet.data[95:64] = tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_data();
+        assert (tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_sop() == 0
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_eop() == 1
+                && tb.dircc_avalon_st_packet_sender_inst_output_bfm.get_transaction_empty() == 0)
+        else begin
+            $sformat(message, "%m: - TEST ERROR: Word 7 Metadata mismatch");
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end
+    endtask : receivePacket
+
+    task automatic checkPacket(packet_t a, packet_t b, inout bool rv);
+        // Dest Address
+        assert (a.dest_addr.hw_addr == b.dest_addr.hw_addr)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Dest HW Addr mismatch %d != %d", a.dest_addr.hw_addr, b.dest_addr.hw_addr);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.dest_addr.sw_addr == b.dest_addr.sw_addr)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Dest SW Addr mismatch %d != %d", a.dest_addr.sw_addr, b.dest_addr.sw_addr);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.dest_addr.port == b.dest_addr.port)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Dest Port mismatch %d != %d", a.dest_addr.port, b.dest_addr.port);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.dest_addr.flag == b.dest_addr.flag)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Dest Flag mismatch %d != %d", a.dest_addr.flag, b.dest_addr.flag);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+
+        // Src Address
+        assert (a.src_addr.hw_addr == b.src_addr.hw_addr)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Src HW Addr mismatch %d != %d", a.src_addr.hw_addr, b.src_addr.hw_addr);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+
+        assert (a.src_addr.sw_addr == b.src_addr.sw_addr)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Src SW Addr mismatch %d != %d", a.src_addr.sw_addr, b.src_addr.sw_addr);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.src_addr.port == b.src_addr.port)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Src Port mismatch %d != %d", a.src_addr.port, b.src_addr.port);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+        assert (a.src_addr.flag == b.src_addr.flag)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Src Flag mismatch %d != %d", a.src_addr.flag, b.src_addr.flag);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+
+        // lamport
+        assert (a.lamport == b.lamport)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Lamport mismatch %d != %d", a.lamport, b.lamport);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
+
+        // Data
+        assert (a.data == b.data)
+        else begin 
+            $sformat(message, "%m: - TEST ERROR: Data mismatch %d != %d", a.data, b.data);
+            print(VERBOSITY_ERROR, message);
+            rv = FALSE;
+        end  
 
     endtask : checkPacket
 
@@ -114,23 +244,22 @@ module dircc_avalon_st_packet_sender_test;
     endtask : sendPacket
 
     task automatic test_resetCorrect();
+        automatic bool rv = TRUE;
+
+        $sformat(message,  "%m: -  Starting Test %m");
+        print(VERBOSITY_INFO, message);
         setupTest();
+
         tb.dircc_avalon_st_packet_sender_inst_reset_bfm.reset_assert();
-        assert (!tb.dircc_avalon_st_packet_sender_inst_output_valid && !tb.dircc_avalon_st_packet_sender_inst_sending) begin
-            $sformat(message, "%m: - TEST SUCCESS");
-            print(VERBOSITY_INFO, message);  
-        end
-        else begin
-            $sformat(message, "%m: - TEST ERROR");
-            print(VERBOSITY_ERROR, message);  
-        end
-        $sformat(message, "%m: - Test Complete.");
-        print(VERBOSITY_INFO, message);  
+        printAssert(tb.dircc_avalon_st_packet_sender_inst_output_valid, 0, rv);
+        printAssert(tb.dircc_avalon_st_packet_sender_inst_sending, 0, rv);
+
+        printSuccess(rv);
     endtask : test_resetCorrect
 
     task automatic test_sendPacket();
-        automatic bool rv = FALSE;
-        automatic bit [31:0] data;
+        automatic bool rv = TRUE;
+        automatic packet_t received_packet;
         automatic packet_t packetToSend = '{
             dest_addr: '{
                 hw_addr: $random,
@@ -147,20 +276,61 @@ module dircc_avalon_st_packet_sender_test;
             lamport: 0,
             data: $random
         };
+
+        $sformat(message,  "%m: -  Starting Test %m");
+        print(VERBOSITY_INFO, message);
         setupTest();
+
         sendPacket(packetToSend);
         waitForMessage(TIMEOUT, rv);
-        if(rv == TRUE) begin
-            checkPacket(packetToSend);
-            $sformat(message, "%m: - TEST SUCCESS");
-            print(VERBOSITY_INFO, message);  
-        end else begin
-            $sformat(message, "%m: - TEST ERROR");
-            print(VERBOSITY_ERROR, message);  
-        end
-        $sformat(message, "%m: - Test Complete.");
-        print(VERBOSITY_INFO, message);  
+
+        receivePacket(received_packet, rv);
+        checkPacket(packetToSend, received_packet, rv);
+
+        printSuccess(rv);
     endtask : test_sendPacket
+
+    task automatic test_backPressure();
+        automatic bool rv = TRUE;
+        automatic packet_t received_packet;
+        automatic packet_t packetToSend = '{
+            dest_addr: '{
+                hw_addr: $random,
+                sw_addr: $random,
+                port: $random,
+                flag: 0
+            },
+            src_addr: '{
+                hw_addr: $random,
+                sw_addr: $random,
+                port: $random,
+                flag: 0
+            },
+            lamport: 0,
+            data: $random
+        };
+        
+        $sformat(message,  "%m: -  Starting Test %m");
+        print(VERBOSITY_INFO, message);
+        setupTest();
+
+        sendPacket(packetToSend);
+        repeat(3) @(posedge clk);
+
+        tb.dircc_avalon_st_packet_sender_inst_output_bfm.set_ready(0);
+
+        repeat(10) @(posedge clk);
+
+
+        tb.dircc_avalon_st_packet_sender_inst_output_bfm.set_ready(1);
+
+        repeat(10) @(posedge clk);
+
+        receivePacket(received_packet, rv);
+        checkPacket(packetToSend, received_packet, rv);
+
+        printSuccess(rv);
+    endtask : test_backPressure
 
     initial begin
         repeat(100) @(posedge clk);
@@ -168,6 +338,7 @@ module dircc_avalon_st_packet_sender_test;
         print(VERBOSITY_INFO, message);  
         test_resetCorrect();
         test_sendPacket();
+        test_backPressure();
     end
 
 endmodule : dircc_avalon_st_packet_sender_test
